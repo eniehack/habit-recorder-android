a8befa951f4c0d1cac6eb8941ed6f929b573326e
Nakaya <eniehack@outlook.jp>
2025-11-18 00:12:03 +0900

    fix: testが落ちてた

--- app/src/test/java/net/eniehack/habitrecorder/ExampleUnitTest.kt
+++ app/src/test/java/net/eniehack/habitrecorder/ExampleUnitTest.kt
@@ -2,9 +2,9 @@ package net.eniehack.habitrecorder
 
 import net.eniehack.habitrecorder.data.CheckIn
 import net.eniehack.habitrecorder.ui.calcStreaks
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.time.Instant
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -23,36 +23,74 @@ class ExampleUnitTest {
 class ExampleStreakTest {
     @Test
     fun testCalcStreak1() {
-        val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val dateFormat = DateTimeFormatter.ofPattern("yyyyMMdd")
+        val now = Instant.now()
         val checkIns = listOf(
-            CheckIn(habitId = 1, createdAt = "2000-01-01"),
-            CheckIn(habitId = 1, createdAt = "2000-01-02"),
-            CheckIn(habitId = 1, createdAt = "2000-01-03")
+            CheckIn(
+                habitId = 1,
+                date = "20000101",
+                createdAt = now,
+                updatedAt = now
+            ),
+            CheckIn(
+                habitId = 1,
+                date = "20000102",
+                createdAt = now,
+                updatedAt = now
+            ),
+            CheckIn(
+                habitId = 1,
+                date = "20000103",
+                createdAt = now,
+                updatedAt = now
+            )
         )
         assertEquals(3, calcStreaks(checkIns, LocalDate.of(2000, 1, 3), dateFormat))
     }
 
     @Test
     fun testCalcStreak2() {
-        val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val dateFormat = DateTimeFormatter.ofPattern("yyyyMMdd")
         val checkIns = listOf<CheckIn>()
         assertEquals(0, calcStreaks(checkIns, LocalDate.of(2000, 1, 3), dateFormat))
     }
     @Test
     fun testCalcStreak3() {
-        val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val dateFormat = DateTimeFormatter.ofPattern("yyyyMMdd")
+        val now = Instant.now()
         val checkIns = listOf(
-            CheckIn(habitId = 1, createdAt = "2000-01-01"),
-            CheckIn(habitId = 1, createdAt = "2000-01-03")
+            CheckIn(
+                habitId = 1,
+                date = "20000101",
+                createdAt = now,
+                updatedAt = now
+            ),
+            CheckIn(
+                habitId = 1,
+                date = "20000103",
+                createdAt = now,
+                updatedAt = now
+            )
         )
         assertEquals(0, calcStreaks(checkIns, LocalDate.of(2000, 1, 4), dateFormat))
     }
     @Test
     fun testCalcStreak4() {
-        val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val dateFormat = DateTimeFormatter.ofPattern("yyyyMMdd")
+        val now = Instant.now()
         val checkIns = listOf(
-            CheckIn(habitId = 1, createdAt = "2000-01-01"),
-            CheckIn(habitId = 1, createdAt = "2000-01-03")
+            CheckIn(
+                habitId = 1,
+                date = "20000101",
+                createdAt = now,
+                updatedAt = now
+            ),
+            CheckIn(
+                habitId = 1,
+                date = "20000103",
+                createdAt = now,
+                updatedAt = now
+            )
         )
         assertEquals(1, calcStreaks(checkIns, LocalDate.of(2000, 1, 3), dateFormat))
     }
