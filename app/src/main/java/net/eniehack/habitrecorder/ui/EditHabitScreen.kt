5bccf5069346642c76babc2fcad7bc6bd0d9caa6
Nakaya <eniehack@outlook.jp>
2025-11-19 23:39:32 +0900

    feat(edithabit): 説明の追加

--- app/src/main/java/net/eniehack/habitrecorder/ui/EditHabitScreen.kt
+++ app/src/main/java/net/eniehack/habitrecorder/ui/EditHabitScreen.kt
@@ -1,5 +1,6 @@
 package net.eniehack.habitrecorder.ui
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,8 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import net.eniehack.habitrecorder.R
 import net.eniehack.habitrecorder.data.Habit
 import net.eniehack.habitrecorder.data.HabitType
 
@@ -18,6 +21,7 @@ import net.eniehack.habitrecorder.data.HabitType
 fun EditHabitScreen(
     habit: Habit,
     enabledPixelaIntegration: Boolean = true,
+    @StringRes submitButtonLabel: Int = R.string.add_habit_submit_button_label,
     onButtonClick: () -> Unit = {},
     onPixelaIdChanged: (String?) -> Unit = {},
     onTitleChanged: (String) -> Unit = {},
@@ -33,25 +37,29 @@ fun EditHabitScreen(
         TextField(
             value = habit.title,
             onValueChange = { onTitleChanged(it) },
-            label = { Text("title") }
+            label = { Text("title") },
+            placeholder = { Text("50ページ読書する") },
+            supportingText = { Text("「今日はこの習慣を達成できた」と判断できる明確な目標を入力してください。") }
         )
         TextField(
             value = habit.unit,
             onValueChange = { onUnitChanged(it) },
             label = { Text("unit") },
+            singleLine = true,
         )
         TextField(
             value = habit.pixelaId ?: "",
             enabled = enabledPixelaIntegration,
             onValueChange = { onPixelaIdChanged(it) },
-            label = { Text("pixela id") }
+            label = { Text("pixela id") },
+            supportingText = { Text("Pixelaに登録する場合、あらかじめ登録したidを入力してください。") }
         )
         Button(
             onClick = {
                 onButtonClick()
             }
         ) {
-            Text("Add")
+            Text(text = stringResource(submitButtonLabel))
         }
     }
 }
